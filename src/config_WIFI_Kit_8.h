4e5ae384085833d519446d8f99174a0b74484f79
Benjamin Dahlhoff <benjamin@dahlhoff.com>
2021-01-14 17:54:56 +0100

    PMS config separated for different devices

--- src/config_WIFI_Kit_8.h
+++ src/config_WIFI_Kit_8.h
@@ -1,4 +1,9 @@
 #include <cstdint>
+
+/* PMS5003 Sensor */
+#define     PMS_RX_PIN              10               // Rx from PMS (== PMS Tx)
+#define     PMS_TX_PIN              12               // Tx to PMS (== PMS Rx)
+
 // Look up the GPIO PINs for the board here: 
 // https://resource.heltec.cn/download/WiFi_Kit_8/WIFI_Kit_8_Pinout_Diagram.pdf
 uint32_t g_CCS811_wake_pin = 14;            // Port to which WAKE pin is connected
--- src/config_WIFI_LoRa_32.h
+++ src/config_WIFI_LoRa_32.h
@@ -1,2 +1,7 @@
 #include <cstdint>
+
+/* PMS5003 Sensor */
+#define     PMS_RX_PIN              22               // Rx from PMS (== PMS Tx)
+#define     PMS_TX_PIN              23               // Tx to PMS (== PMS Rx)
+
 uint32_t g_CCS811_wake_pin = 2;            // Port to which WAKE pin is connected 
